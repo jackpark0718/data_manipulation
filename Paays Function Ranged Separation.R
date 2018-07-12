6d242363862be9b128399abf9c975caa40b03cc7
Jack Park <jpark@tfs.ca>
2018-07-12 10:03:33 -0400

    updated code and graphs included

--- Paays Function Ranged Separation.R
+++ Paays Function Ranged Separation.R
@@ -1,6 +1,9 @@
 library(tidyr)
 setwd("/Users/jackpark/Desktop") #set your directory
-wishpond <- read.csv("wishpond_leads_june25 - wishpond_leads_june25.csv")
+wishpond <- read.csv("Paays July 10th.csv")
+wishpond$finance_range <- wishpond$Whatis.The.Desired.Amount.You.Would.Want.To.Finance.
+wishpond$vacation_budget <- wishpond$How.much.do.you.typically.spend.on.a.vacation.package.
+wishpond$leisure_travel_type <- wishpond$What.does.your.dream.vacation.look.like...select.all.that.apply.
 #there are   options in regards to what the finance range can be, so I can just replace
 #each option with whatever is the average of its range and for $10,000+ I'll just set the value to 10000
 ave.finance_range <- function(wishpond, finance_range){
@@ -86,29 +89,29 @@ ave.age_range <- function(wishpond, age_range){
 #Splitting up the leisure_travel_type into different columns that give either 1 or 0 based on if the user 
 #selected that value, so 1 if they selected that option and 0 if not
 wishpond$leisure_travel_type_Beach <- 0
-wishpond$leisure_travel_type_Sightseeing <- 0
+wishpond$leisure_travel_type_Sightseeing_Touring <- 0
 wishpond$leisure_travel_type_Trekking <- 0
-wishpond$leisure_travel_type_Touring <- 0
+wishpond$leisure_travel_type_Skiing_Golfing_Biking <- 0
 wishpond$leisure_travel_type_Other <- 0
 for(l in 1:nrow(wishpond)){
   if(grepl("Beach", wishpond$leisure_travel_type[l], fixed = TRUE)){
     wishpond$leisure_travel_type_Beach[l] <- 1
   }
   if(grepl("Sightseeing", wishpond$leisure_travel_type[l], fixed = TRUE)){
-    wishpond$leisure_travel_type_Sightseeing[l] <- 1
+    wishpond$leisure_travel_type_Sightseeing_Touring[l] <- 1
   }
   if(grepl("Trekking", wishpond$leisure_travel_type[l], fixed = TRUE)){
     wishpond$leisure_travel_type_Trekking[l] <- 1
   }
-  if(grepl("Touring", wishpond$leisure_travel_type[l], fixed = TRUE)){
-    wishpond$leisure_travel_type_Touring[l] <- 1
+  if(grepl("Golfing", wishpond$leisure_travel_type[l], fixed = TRUE)){
+    wishpond$leisure_travel_type_Skiing_Golfing_Biking[l] <- 1
   }
   if(grepl("Other", wishpond$leisure_travel_type[l], fixed = TRUE)){
     wishpond$leisure_travel_type_Other[l] <- 1
   }
 }
 library(scales)
-clean_data <- subset(wishpond, select = c("Email", "leisure_travel_type_Beach", "leisure_travel_type_Sightseeing", "leisure_travel_type_Trekking", "leisure_travel_type_Touring", "leisure_travel_type_Other", "ave_vacation_budget", "ave_finance_range", "ave_age_range"))
+clean_data <- subset(wishpond, select = c("Email", "leisure_travel_type_Beach", "leisure_travel_type_Sightseeing_Touring", "leisure_travel_type_Trekking", "leisure_travel_type_Skiing_Golfing_Biking", "leisure_travel_type_Other", "ave_vacation_budget", "ave_finance_range", "ave_age_range"))
 clean_data$ave_vacation_budget <- type.convert(clean_data$ave_vacation_budget, na.strings = "NA", dec=".", numerals = c("no.loss"))
 clean_data$ave_finance_range <- type.convert(clean_data$ave_finance_range, na.strings = "NA", dec=".", numerals = c("no.loss"))
 clean_data$ave_age_range <- type.convert(clean_data$ave_age_range, na.strings = "NA", dec=".", numerals = c("no.loss"))
@@ -116,58 +119,82 @@ clean_data <- subset(clean_data, !is.na(ave_vacation_budget))
 clean_data <- subset(clean_data, !is.na(ave_finance_range))
 clean_data <- subset(clean_data, !is.na(ave_age_range))
 #limiting data to the pertinent columns and getting rid of rows with limited data
-budget1000 <- subset(clean_data, ave_vacation_budget==1000)
-budget1500 <- subset(clean_data, ave_vacation_budget==1500)
-budget3500 <- subset(clean_data, ave_vacation_budget==3500)
-budget7500 <- subset(clean_data, ave_vacation_budget==7500)
-budget10000 <- subset(clean_data, ave_vacation_budget==10000)
+Leisure_Options <- c("Beach", "Sightseeing", "Trekking", "Skiing", "Other")
+BudgetOptions <- c(1000, 1500, 3500, 7500, 10000)
+data.split <- function(BudgetOptions, n){
+  for(j in 1:n){
+    bud <- BudgetOptions[j]
+    y <- paste("budget", bud, sep = "", collapse = NULL)
+    assign(x=y, value = subset(clean_data, ave_vacation_budget==bud))
+    for(i in 1:5){
+      z <- paste("budget", bud, "_", Leisure_Options[i], sep = "", collapse = NULL)
+      b <- paste("leisure_travel_type_", Leisure_Options[i], sep ="", collapse = NULL)
+      c <- subset(clean_data, ave_vacation_budget==bud)
+      c <- c[i+1]
+      assign(x=z, value = percent(sum(c)/nrow(c)))
+    }
+  }
+}
 budget5000plus <- subset(clean_data, ave_vacation_budget>=5000)
 #split the data into each different price range to compare all of the ranges
-budget1000_beach <- percent(sum(budget1000$leisure_travel_type_Beach)/nrow(budget1000))
-budget1000_sightseeing <- percent(sum(budget1000$leisure_travel_type_Sightseeing)/nrow(budget1000))
-budget1000_trekking <- percent(sum(budget1000$leisure_travel_type_Trekking)/nrow(budget1000))
-budget1000_touring <- percent(sum(budget1000$leisure_travel_type_Touring)/nrow(budget1000))
-budget1000_other <- percent(sum(budget1000$leisure_travel_type_Other)/nrow(budget1000))
-budget1500_beach <- percent(sum(budget1500$leisure_travel_type_Beach)/nrow(budget1500))
-budget1500_sightseeing <- percent(sum(budget1500$leisure_travel_type_Sightseeing)/nrow(budget1500))
-budget1500_trekking <- percent(sum(budget1500$leisure_travel_type_Trekking)/nrow(budget1500))
-budget1500_touring <- percent(sum(budget1500$leisure_travel_type_Touring)/nrow(budget1500))
-budget1500_other <- percent(sum(budget1500$leisure_travel_type_Other)/nrow(budget1500))
-budget3500_beach <- percent(sum(budget3500$leisure_travel_type_Beach)/nrow(budget3500))
-budget3500_sightseeing <- percent(sum(budget3500$leisure_travel_type_Sightseeing)/nrow(budget3500))
-budget3500_trekking <- percent(sum(budget3500$leisure_travel_type_Trekking)/nrow(budget3500))
-budget3500_touring <- percent(sum(budget3500$leisure_travel_type_Touring)/nrow(budget3500))
-budget3500_other <- percent(sum(budget3500$leisure_travel_type_Other)/nrow(budget3500))
-budget7500_beach <- percent(sum(budget7500$leisure_travel_type_Beach)/nrow(budget7500))
-budget7500_sightseeing <- percent(sum(budget7500$leisure_travel_type_Sightseeing)/nrow(budget7500))
-budget7500_trekking <- percent(sum(budget7500$leisure_travel_type_Trekking)/nrow(budget7500))
-budget7500_touring <- percent(sum(budget7500$leisure_travel_type_Touring)/nrow(budget7500))
-budget7500_other <- percent(sum(budget7500$leisure_travel_type_Other)/nrow(budget7500))
-budget10000_beach <- percent(sum(budget10000$leisure_travel_type_Beach)/nrow(budget10000))
-budget10000_sightseeing <- percent(sum(budget10000$leisure_travel_type_Sightseeing)/nrow(budget10000))
-budget10000_trekking <- percent(sum(budget10000$leisure_travel_type_Trekking)/nrow(budget10000))
-budget10000_touring <- percent(sum(budget10000$leisure_travel_type_Touring)/nrow(budget10000))
-budget10000_other <- percent(sum(budget10000$leisure_travel_type_Other)/nrow(budget10000))
-budget5000plus_beach <- percent(sum(budget5000plus$leisure_travel_type_Beach)/nrow(budget5000plus))
-budget5000plus_sightseeing <- percent(sum(budget5000plus$leisure_travel_type_Sightseeing)/nrow(budget5000plus))
-budget5000plus_trekking <- percent(sum(budget5000plus$leisure_travel_type_Trekking)/nrow(budget5000plus))
-budget5000plus_touring <- percent(sum(budget5000plus$leisure_travel_type_Touring)/nrow(budget5000plus))
-budget5000plus_other <- percent(sum(budget5000plus$leisure_travel_type_Other)/nrow(budget5000plus))
-total_budget_beach <- percent(sum(clean_data$leisure_travel_type_Beach)/nrow(clean_data))
-total_budget_sightseeing <- percent(sum(clean_data$leisure_travel_type_Sightseeing)/nrow(clean_data))
-total_budget_trekking <- percent(sum(clean_data$leisure_travel_type_Trekking)/nrow(clean_data))
-total_budget_touring <- percent(sum(clean_data$leisure_travel_type_Touring)/nrow(clean_data))
-total_budget_other <- percent(sum(clean_data$leisure_travel_type_Other)/nrow(clean_data))
+for (k in 1:5) {
+  z <- paste("budget5000plus_", Leisure_Options[k], sep = "", collapse = NULL)
+  c <- budget5000plus[k+1]
+  assign(x=z, value = percent(sum(c)/nrow(c)))
+}
+for (m in 1:5) {
+  z <- paste("total_budget_", Leisure_Options[m], sep = "", collapse = NULL)
+  c <- clean_data[m+1]
+  assign(x=z, value = percent(sum(c)/nrow(c)))
+}
 #find the percentage chance that each option of leisure travel was selected for the budget groups and for the group as a whole
-Groups <- c("<$1000","$1,000-$2,000", "$2,000-$5,000", "$5,000-$10,000", "$10,000+", "$5,000+", "Everyone")
-Beach <- c(budget1000_beach, budget1500_beach, budget3500_beach, budget7500_beach, budget10000_beach, budget5000plus_beach, total_budget_beach)
-Sightseeing <- c(budget1000_sightseeing, budget1500_sightseeing, budget3500_sightseeing, budget7500_sightseeing, budget10000_sightseeing, budget5000plus_sightseeing, total_budget_sightseeing)
-Trekking <- c(budget1000_trekking, budget1500_trekking, budget3500_trekking, budget7500_trekking, budget10000_trekking, budget5000plus_trekking, total_budget_trekking)
-Touring <- c(budget1000_touring, budget1500_touring, budget3500_touring, budget7500_touring, budget10000_touring, budget5000plus_touring, total_budget_touring)
-Other <- c(budget1000_other, budget1500_other, budget3500_other, budget7500_other, budget10000_other, budget5000plus_other, total_budget_other)
+Groups <- c(rep("<$1,000", 5), rep("$1,000-$2,000", 5), rep("$2,000-$5,000", 5), rep("$5,000-$10,000", 5), rep("$10,000+", 5), rep("$5,000+", 5), rep("Everyone", 5))
 Occurances <- c(nrow(budget1000), nrow(budget1500), nrow(budget3500), nrow(budget7500), nrow(budget10000), nrow(budget5000plus), nrow(clean_data))
-budgetcomparison <- data.frame(Groups, Beach, Sightseeing, Trekking, Touring, Other, Occurances)
-budgetcomparison
-#as we can see from this table the beach, sightseeing and touring are by far the three most saught after leasure travel types
-#as a general trend, sightseeing, trekking, touring and other all decrease with increasing budget
-#we also see that there are less options selected the higher the budget and the higher budget users tend to gravitate more towards the beach
+Leisure_Activities <- rep(c("Beach", "Sightseeing and Touring", "Trekking", "Skiing, Golfing, Biking, etc...", "Other"), 7)
+Percentage_Wanted <- c(budget1000_Beach, budget1000_Sightseeing, budget1000_Trekking, budget1000_Skiing, budget1000_Other, budget1500_Beach, budget1500_Sightseeing, budget1500_Trekking, budget1500_Skiing, budget1500_Other, budget3500_Beach, budget3500_Sightseeing, budget3500_Trekking, budget3500_Skiing, budget3500_Other, budget7500_Beach, budget7500_Sightseeing, budget7500_Trekking, budget7500_Skiing, budget7500_Other, budget10000_Beach, budget10000_Sightseeing, budget10000_Trekking, budget10000_Skiing, budget10000_Other, budget5000plus_Beach, budget5000plus_Sightseeing, budget5000plus_Trekking, budget5000plus_Skiing, budget5000plus_Other, total_budget_Beach, total_budget_Sightseeing, total_budget_Trekking, total_budget_Skiing, total_budget_Other)
+budgetcomparison <- data.frame(Groups, Leisure_Activities, Percentage_Wanted)
+Groups2 <- factor(Groups, levels = c("<$1,000", "$1,000-$2,000", "$2,000-$5,000", "$5,000-$10,000", "$5,000+", "$10,000+", "Everyone"))
+library(ggplot2)
+ggplot(budgetcomparison, aes(fill = Leisure_Activities, y = Percentage_Wanted, x = Groups2)) + geom_bar(position = "dodge", stat = "identity") + ggtitle("Budget Groups Preferred Leisure Activities") + labs(x = "Budget Groups", y = "Percentage of Users Preferred")
+Age_Options <- unique(clean_data$ave_age_range, incomparables = FALSE)
+Age_Options <- sort(Age_Options, decreasing = FALSE)
+freq1000 <- as.data.frame(table(budget1000$ave_age_range))
+freq1500 <- as.data.frame(table(budget1500$ave_age_range))
+freq3500 <- as.data.frame(table(budget3500$ave_age_range))
+freq7500 <- as.data.frame(table(budget7500$ave_age_range))
+freq10000 <- as.data.frame(table(budget10000$ave_age_range))
+freq_total <- as.data.frame(table(clean_data$ave_age_range))
+#will have to be changed with more data because there may no longer be frequencies of 0 for some
+Frequencies <- c(freq1000$Freq[1], freq1000$Freq[2], freq1000$Freq[3], freq1000$Freq[4], freq1000$Freq[5], freq1000$Freq[6], freq1000$Freq[7], freq1500$Freq[1], freq1500$Freq[2], freq1500$Freq[3], freq1500$Freq[4], freq1500$Freq[5], freq1500$Freq[6], freq1500$Freq[7], freq3500$Freq[1], freq3500$Freq[2], freq3500$Freq[3], freq3500$Freq[4], freq3500$Freq[5], freq3500$Freq[6], freq3500$Freq[7], freq7500$Freq[1], freq7500$Freq[2], freq7500$Freq[3], freq7500$Freq[4], freq7500$Freq[5], freq7500$Freq[6], 0, freq10000$Freq[1], 0, freq10000$Freq[2], freq10000$Freq[3], freq10000$Freq[4], freq10000$Freq[5], 0, freq_total$Freq[1], freq_total$Freq[2], freq_total$Freq[3], freq_total$Freq[4], freq_total$Freq[5], freq_total$Freq[6], freq_total$Freq[7])
+Budget <- c(rep("<$1,000", 7), rep("$1,000-$2,000", 7), rep("$2,000-$5,000", 7), rep("$5,000-$10,000", 7), rep("$10,000+", 7), rep("Everyone", 7))
+Ages <- rep(Age_Options, 6)
+AgeVaca <- data.frame(Ages, Budget, Frequencies)
+AgeVaca$Budget2 <- factor(AgeVaca$Budget, levels = c("<$1,000", "$1,000-$2,000", "$2,000-$5,000", "$5,000-$10,000", "$10,000+", "Everyone"))
+ggplot(AgeVaca, aes(fill = Ages, y = Frequencies, x = Budget2)) + geom_bar(position = "stack", stat = "identity") + ggtitle("Budget Groups by Age") + labs(x = "Budget Groups", y = "Number of Users")
+data.split <- function(Age_Options, n){
+  for(j in 1:n){
+    age <- Age_Options[j]
+    y <- paste("age", age, sep = "", collapse = NULL)
+    assign(x=y, value = subset(clean_data, ave_age_range==age))
+    for(i in 1:5){
+      z <- paste("age", age, "_", Leisure_Options[i], sep = "", collapse = NULL)
+      b <- paste("leisure_travel_type_", Leisure_Options[i], sep ="", collapse = NULL)
+      c <- subset(clean_data, ave_age_range==age)
+      c <- c[i+1]
+      assign(x=z, value = sum(c)/nrow(c)*100)
+    }
+  }
+}
+Age_Possibilities <- c(21, 29.5, 39.5, 49.5, 59.5, 69.5, 75)
+Age_Beach <- c(age21_Beach, age29.5_Beach, age39.5_Beach, age49.5_Beach, age59.5_Beach, age69.5_Beach, age75_Beach)
+Age_Sightseeing <- c(age21_Sightseeing, age29.5_Sightseeing, age39.5_Sightseeing, age49.5_Sightseeing, age59.5_Sightseeing, age69.5_Sightseeing, age75_Sightseeing)
+Age_Trekking <- c(age21_Trekking, age29.5_Trekking, age39.5_Trekking, age49.5_Trekking, age59.5_Trekking, age69.5_Trekking, age75_Trekking)
+Age_Skiing <- c(age21_Skiing, age29.5_Skiing, age39.5_Skiing, age49.5_Skiing, age59.5_Skiing, age69.5_Skiing, age75_Skiing)
+Age_Other <- c(age21_Other, age29.5_Other, age39.5_Other, age49.5_Other, age59.5_Other, age69.5_Other, age75_Other)
+Age_Data <- data.frame(Age_Possibilities, Age_Beach, Age_Sightseeing, Age_Trekking, Age_Skiing, Age_Other)
+plot(type = "l", x = Age_Data$Age_Possibilities, y = Age_Data$Age_Beach, main = "Leisure Types by Age", xlab = "Age", ylab = "Preferred Leisure Type (%)", xlim = c(18, 80), ylim = c(0, 100), col = "red", lwd = 2) 
+lines(x = Age_Data$Age_Possibilities, y = Age_Data$Age_Sightseeing, type = "l", col = "yellow", lwd = 2)
+lines(x = Age_Data$Age_Possibilities, y = Age_Data$Age_Trekking, type = "l", col = "green", lwd = 2)
+lines(x = Age_Data$Age_Possibilities, y = Age_Data$Age_Skiing, type = "l", col = "blue", lwd = 2)
+lines(x = Age_Data$Age_Possibilities, y = Age_Data$Age_Other, type = "l", col = "purple", lwd = 2)
+legend("topright", legend = c("Beach", "Sightseeing and Touring", "Trekking", "Skiing, Golfing, Biking, etc...", "Other"), col = c("red", "yellow", "green", "blue", "purple"), lty = 1, cex = 0.6, title = "Leisure Types", text.font = 4, box.lty = 1, lwd = 2, bg = "light blue", box.col = "light blue")
